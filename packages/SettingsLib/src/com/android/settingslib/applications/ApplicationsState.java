fd1d590f3bddfadda6db6911d353c96fd8e69365
George G <kreach3r@users.noreply.github.com>
2016-07-04 06:25:15 +0300

    ApplicationsState: add filter for Substratum overlays [2/2]
    This commit allows the framework to handle the filtering of the
    overlays found for OMS.
    
    Change-Id: I7646115e8f73494d726728fac58cc47aafd69d5d
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
@@ -210,9 +210,11 @@ public class ApplicationsState {
         // Only the owner can see all apps.
         mAdminRetrieveFlags = PackageManager.MATCH_ANY_USER |
                 PackageManager.MATCH_DISABLED_COMPONENTS |
-                PackageManager.MATCH_DISABLED_UNTIL_USED_COMPONENTS;
+                PackageManager.MATCH_DISABLED_UNTIL_USED_COMPONENTS |
+                PackageManager.GET_META_DATA;
         mRetrieveFlags = PackageManager.MATCH_DISABLED_COMPONENTS |
-                PackageManager.MATCH_DISABLED_UNTIL_USED_COMPONENTS;
+                PackageManager.MATCH_DISABLED_UNTIL_USED_COMPONENTS |
+                PackageManager.GET_META_DATA;
 
         final List<ModuleInfo> moduleInfos = mPm.getInstalledModules(0 /* flags */);
         for (ModuleInfo info : moduleInfos) {
@@ -1709,6 +1711,18 @@ public class ApplicationsState {
         public boolean filterApp(AppEntry entry) {
             return entry.info.enabledSetting
                     != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_UNTIL_USED;
+        }
+    };
+
+    public static final AppFilter FILTER_SUBSTRATUM = new AppFilter() {
+        public void init() {
+        }
+
+        @Override
+        public boolean filterApp(AppEntry entry) {
+            return !(entry.info.metaData != null &&
+                     (entry.info.metaData.getString("Substratum_Parent") != null ||
+                      entry.info.metaData.getString("Lite_Version") != null));
         }
     };
 
